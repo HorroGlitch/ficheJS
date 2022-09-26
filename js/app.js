008ac06bf93e38fee9aad694e1207b1b275962d4
HorroGlitch <lverot@edenschool.fr>
2022-09-26 09:57:04 +0200

    fix

---
+++ js/app.js
@@ -0,0 +1,46 @@
+console.log("hello");
+let age;
+age = '16';
+
+//age est un stand//
+
+const prenom= 'Loïc'
+//Prenom est une constante
+
+let addition;
+addition = 4+12;
+console.log(addition)
+
+let soustraction;
+soustraction = 2-50;
+console.log(soustraction)
+
+let multiplication;
+multiplication = 3*8;
+console.log(multiplication)
+
+let division;
+division = 7/2;
+console.log(division)
+
+let modulo;
+modulo = 5%2;
+console.log(modulo)
+
+let test;
+test = 3>5;
+console.log(test)
+
+console.log(typeof(test))
+
+//Test est un boolean
+
+// >  >=  <  <=  ==  ===  !=  !==
+
+const identity= "bonjour" + " " + "Loïc" + " " + "vous avez" + " " + "16" + " " + "ans"
+console.log(identity)
+
+//L'opériation qui est réalisée est une concaténation
+
+let majuscule = identity.toUpperCase();
+console.log(majuscule)
